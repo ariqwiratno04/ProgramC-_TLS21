0c8ed5683bda285e4cd641b91decadfb118a4407
ariqwiratno04 <92524664+ariqwiratno04@users.noreply.github.com>
2021-10-14 19:27:14 +0700

    Add files via upload

---
+++ Tugas TLS.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+ //mesin kasir
+
+
+int main()
+{
+    int jumlah_barang, i, harga_item[10], jumlah_item[10], sub_total[10];
+    string nama_item[10];
+    int total;
+
+    cout << "DAFTAR BELANJA C++" << endl;
+    cout << "---------------------- \n";
+    cout << "Masukkan jumlah barang yang dibeli: ";
+    cin >> jumlah_barang;
+    cout << endl;
+
+    for(i = 0; i < jumlah_barang; i++){
+        cout << "Masukkan nama item ke " << i + 1 << ": ";
+        cin >> nama_item[i];
+
+        cout << "Masukkan harga item (dalam rupiah): ";
+        cin >> harga_item[i];
+
+        cout << "Masukkan jumlah item yang akan dibeli: ";
+        cin >> jumlah_item[i];
+        cout << endl;
+
+        sub_total[i] = jumlah_item[i] * harga_item[i];
+        total += sub_total[i];
+
+    }
+
+    cout << endl;
+    cout << "LIST BARANG YANG DIBELI" << endl;
+    cout << "------------------------------------------------ \n";
+    cout << "------------------------------------------------ \n";
+    cout << "No     " << "Nama Barang   " << "Jumlah    " << "Harga     " << "Sub Total     " << endl;
+    cout << "------------------------------------------------ \n";
+    for (i = 0; i < jumlah_barang; i++){
+    cout << i + 1 << setw(12) << nama_item[i] << setw(11) << jumlah_item[i] << setw(12) << harga_item[i] << setw(11) << sub_total[i] << endl;;
+    }
+    cout << endl;
+    cout << "Uang yang harus disiapkan untuk belanja: Rp." << total <<",00" << endl;
+return 0;
+}
